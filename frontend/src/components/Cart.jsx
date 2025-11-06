5fa48fdf05f1f240c92c1541437afda99941b8f5
ishita jain <ishitajain385@gmail.com>
2025-11-06 17:28:49 +0530

    chnaged to ensure address gets added

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -211,6 +211,9 @@ export default function Cart() {
       console.error(err);
       alert("Something went wrong!");
     }
+    finally{
+      setOtpVerified(false)
+    }
   };
 
   async function proceedToCheckout() {
